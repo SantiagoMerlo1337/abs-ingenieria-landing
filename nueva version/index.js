d7a6bbd414b32bff246530f78ae81e0c84da8330
MNAF1337 <66649631+MNAF1337@users.noreply.github.com>
2022-04-29 10:55:34 -0300

    scrollreveal y poco mas

---
+++ nueva version/index.js
@@ -0,0 +1,17 @@
+window.sr = ScrollReveal();
+  sr.reveal('.navbar', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-100px'
+  });
+
+  sr.reveal('.scroll-reveal', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-100px'
+  });
+  sr.reveal('.card', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-100px'
+  });
