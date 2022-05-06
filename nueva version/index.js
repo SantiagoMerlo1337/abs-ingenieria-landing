37a1749dc69e63e2426d3500570488d7d8af7832
MNAF1337 <66649631+MNAF1337@users.noreply.github.com>
2022-05-06 00:48:21 -0300

    sabe

--- nueva version/index.js
+++ nueva version/index.js
@@ -1,17 +1,17 @@
 window.sr = ScrollReveal();
   sr.reveal('.navbar', {
-    duration: 3000,
+    duration: 1500,
     origin: 'bottom',
     distance: '-100px'
   });
 
   sr.reveal('.scroll-reveal', {
-    duration: 3000,
+    duration: 2500,
     origin: 'bottom',
     distance: '-100px'
   });
   sr.reveal('.card', {
-    duration: 3000,
+    duration: 1000,
     origin: 'bottom',
     distance: '-100px'
   });
